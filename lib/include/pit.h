96b60193f9cd53b8d289f474130b83aa2ec4819e
thatcppnerd <welcometoheccmp4@gmail.com>
2024-04-12 20:56:39 -0500

    added pit libs

---
+++ lib/include/pit.h
@@ -0,0 +1,48 @@
+#ifndef __PIT_H__
+#define __PIT_H__
+
+#include "../dev/types.h"
+
+#define PIT_OSC_FREQ 1193182 // Base oscilator frequency of PIT
+
+// enums for PIT operating modes 
+
+#define PIT_M0          0x00  // Interupt on Terminal Count
+#define PIT_M1          0x01  // Hardware Re-triggerable One-Shot
+#define PIT_M2          0x02  // Rate Generator
+#define PIT_M3          0x03  // Square Wave Generator
+#define PIT_M4          0x04  // Software Triggered Strobe
+#define PIT_M5          0x05  // Hardware Triggered Strobe
+
+// PIT channel enums
+
+#define PIT_CH0         0x00
+#define PIT_CH1         0x01
+#define PIT_CH2         0x02
+
+// PIT I/O ports
+
+#define PIT_CH0_DATA    0x40
+#define PIT_CH1_DATA    0x41
+#define PIT_CH2_DATA    0x42
+#define PIT_MCR         0x43 // to Mode/Command Register
+
+// typedef struct PIT_ReadBack
+// {
+//     u8_t
+//         BCD : 1,
+//         operatingMode : 3,
+//         accessMode : 2,
+//         nullCount : 1,
+//         outputPin : 1;
+// } PIT_ReadBack_t;
+
+void PIT_setReloadValue(u8_t channel, u16_t val);
+u16_t PIT_getCurrentCount(u8_t channel);
+
+void PIT_latchCount(u8_t channel);
+
+void PIT_writeMC(u8_t data);
+
+
+#endif
--- lib/src/pit.c
+++ lib/src/pit.c
@@ -3,16 +3,16 @@
 
 void PIT_setReloadValue(u8_t channel, u16_t val)
 {
-    outb(PIT_PORT_CH0_DATA + channel, val & 0x00FF);
-    outb(PIT_PORT_CH0_DATA + channel, val >> 8);
+    outb(PIT_CH0_DATA + channel, val & 0x00FF);
+    outb(PIT_CH0_DATA + channel, val >> 8);
     return;
 }
 
 u16_t PIT_getCurrentCount(u8_t channel)
 {
     u16_t count = 0;
-    count = inb(PIT_PORT_CH0_DATA + channel);
-    count |= inb(PIT_PORT_CH0_DATA + channel) << 8;
+    count = inb(PIT_CH0_DATA + channel);
+    count |= inb(PIT_CH0_DATA + channel) << 8;
     return count;
 }
 
@@ -24,6 +24,6 @@ void ALWAYSINLINE PIT_latchCount(u8_t channel)
 
 inline void ALWAYSINLINE PIT_writeMCReg(u8_t data)
 {
-    outb(PIT_PORT_MC_REG, data);
+    outb(PIT_MCR, data);
     return;
 }
