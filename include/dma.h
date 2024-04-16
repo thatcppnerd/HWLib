84c80adc1662dd4ac40967c901629cf886e7b9d5
dumpy <112026806+thatcppnerd@users.noreply.github.com>
2024-04-16 12:10:00 -0500

    DMA libs wip

--- include/dma.h
+++ include/dma.h
@@ -3,23 +3,29 @@
 
 #include "types.h"
 #include "asm.h"
+#include "util.h"
 
 // Macros for channel select
 
+#define DMA_CH0 0
+#define DMA_CH1 1
+#define DMA_CH2 2
+#define DMA_CH3 3
+#define DMA_CH4 4
+#define DMA_CH5 5
+#define DMA_CH6 6
+#define DMA_CH7 7
+
+// Macros for chip select
+
 #define DMA0 0
 #define DMA1 1
-#define DMA2 2
-#define DMA3 3
-#define DMA4 4
-#define DMA5 5
-#define DMA6 6
-#define DMA7 7
+
 
 // IO Ports
 
-#define DMA_MASTER_BASE 0x00
-#define DMA_SLAVE_BASE  0xC0
-
+#define DMA0_BASE   0xC0 
+#define DMA1_BASE   0x00
 
 // Register flag enums
 
@@ -27,9 +33,9 @@ enum DMA_
 
 
 
-void DMA_SetStartAddress(unsigned int channel, u16_t addr);
-void DMA_SetCount(unsigned int channe)
-u8_t DMA_GetStatus(void);
+void DMA_SetStartAddress(int channel, u16_t addr);
+void DMA_SetCount(int channel);
+u8_t DMA_GetStatus(int select);
 void DMA_SendCommand(u8_t cmd);
 void DMA_SendRequest(u8_t cmd);
 void DMA_SetMask(u8_t mask);
--- src/dma.c
+++ src/dma.c
@@ -2,8 +2,47 @@
 
 void DMA_SetStartAddress(int channel, u16_t addr)
 {
-    if(channel > DMA4)
+    if(channel < DMA_CH4) // on slave DMA
     {
-        outb(0x00 + channel * 2)
+        outb(DMA0_BASE + channel * 2, addr & 0xFF); // low byte
+        outb(DMA0_BASE + channel * 2, (addr >> 8) & 0xFF); // high byte
+        io_wait();
     }
+    else if(channel >= DMA_CH4) // on master DMA
+    {
+        outb(DMA1_BASE + (channel - 4) * 2, addr & 0xFF); // low byte
+        outb(DMA1_BASE + (channel - 4) * 4, (addr >> 8) & 0xFF); // high byte
+        io_wait();
+    }
+    else return;
+}
+
+void DMA_SetCount(int channel, u16_t addr)
+{
+    if(channel < DMA_CH4) // on slave DMA
+    {
+        outb((DMA0_BASE + 1) + channel * 2, addr & 0xFF); // low byte
+        outb((DMA0_BASE + 1) + channel * 2, (addr >> 8) & 0xFF); // high byte
+        io_wait();
+    }
+    else if(channel >= DMA_CH4) // on master DMA
+    {
+        outb((DMA1_BASE + 2) + (channel - 4) * 4, addr & 0xFF); // low byte
+        outb((DMA1_BASE + 2) + (channel - 4) * 4, (addr >> 8) & 0xFF); // high byte
+        io_wait();
+    }
+    else return;
+}
+
+u8_t DMA_GetStatus(int select)
+{
+    if(select == DMA0)
+    {
+        return inb(DMA0_BASE + 0x10);
+    }
+    else if(select == DMA1)
+    {
+        return inb(DMA1_BASE + 8);
+    }
+    else return;
 }
