a50f3419e4e50c6098c7b06ac1b40fd7a80a2be1
thatcppnerd <welcometoheccmp4@gmail.com>
2024-04-21 20:11:45 -0500

    final fix

--- src/asm.c
+++ src/asm.c
@@ -1,26 +1,26 @@
 #include "../include/asm.h"
 
-volatile inline void ALWAYSINLINE outb(u16_t port, u8_t val)
+inline void ALWAYSINLINE outb(u16_t port, u8_t val)
 {
     register u16_t dx asm("%dx") = port;
 
     asm volatile("outb %0, %1 \n" : : "a"(val), "d"(dx));
 }
 
-volatile inline void ALWAYSINLINE outw(u16_t port, u16_t val)
+inline void ALWAYSINLINE outw(u16_t port, u16_t val)
 {
     register u16_t dx asm("%dx") = port;
 
     asm volatile("outw %0, %1 \n" : : "a"(val), "d"(port));
 }
 
-volatile inline void ALWAYSINLINE outdw(u16_t port, u32_t val)
+inline void ALWAYSINLINE outdw(u16_t port, u32_t val)
 {
     register u32_t eax asm("%eax") = val;
     asm volatile("outl %0, %1 \n" : : "a"(val), "d"(port));
 }
 
-volatile inline u8_t ALWAYSINLINE inb(u16_t port)
+inline u8_t ALWAYSINLINE inb(u16_t port)
 {
     register volatile u8_t al asm("%al");
     asm volatile("inb %0, %%al\n" : : "d"(port) : "al" );
@@ -28,7 +28,7 @@ volatile inline u8_t ALWAYSINLINE inb(u16_t port)
     return al;
 }
 
-volatile inline u16_t ALWAYSINLINE inw(u16_t port)
+inline u16_t ALWAYSINLINE inw(u16_t port)
 {
     register volatile u16_t ax asm("%ax");
     asm volatile("inw %0, %%ax\n" : : "d"(port) : "ax" );
@@ -36,7 +36,7 @@ volatile inline u16_t ALWAYSINLINE inw(u16_t port)
     return ax;
 }
 
-volatile inline u32_t ALWAYSINLINE indw(u16_t port)
+inline u32_t ALWAYSINLINE indw(u16_t port)
 {
     register volatile u32_t eax asm("%eax");
     asm volatile("inl %0, %%eax\n" : : "d"(port) : "eax");
@@ -46,17 +46,17 @@ volatile inline u32_t ALWAYSINLINE indw(u16_t port)
 
 // Asm wrapper defs
 
-volatile inline void ALWAYSINLINE cli(void)
+inline void ALWAYSINLINE cli(void)
 {
     asm volatile ("cli;");
 }
 
-volatile inline void ALWAYSINLINE sti(void)
+inline void ALWAYSINLINE sti(void)
 {
     asm volatile ("sti;");
 }
 
-volatile inline void ALWAYSINLINE hlt(void)
+inline void ALWAYSINLINE hlt(void)
 {
     asm volatile ("hlt;");
 }
