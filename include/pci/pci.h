e5eeb7ecff96246f5a590aba7fbe15b06bcc29fb
dumpy <112026806+thatcppnerd@users.noreply.github.com>
2024-04-23 14:11:14 -0500

    added pci funcs(VERY WIP)

--- include/pci/pci.h
+++ include/pci/pci.h
@@ -1,6 +1,9 @@
 #ifndef __PCI_H__
 #define __PCI_H__
 
+#include "../types.h"
+#include "../macros.h"
+
 enum PCI_Ports
 {
     PCI_CONFIG_ADDRESS =        0xCF8,
@@ -13,9 +16,9 @@ u8_t    PCI_Read8(u32_t addr);
 u16_t   PCI_Read16(u32_t addr);
 u32_t   PCI_Read32(u32_t addr);
 
-u8_t    PCI_Write8(u32_t addr);
-u16_t   PCI_Write16(u32_t addr);
-u32_t   PCI_Write32(u32_t addr);
+void   PCI_Write8(u32_t addr, u8_t val);
+void   PCI_Write16(u32_t addr, u16_t val);
+void   PCI_Write32(u32_t addr, u32_t val);
 
 
 #endif
---
+++ src/pci/pci.c
@@ -0,0 +1,29 @@
+#include "../include/pci.h"
+
+
+u32_t PCI_CreateConfigAddress(u8_t bus, u8_t slot, u8_t func, u8_t offset)
+{
+    u32_t addr = (0x00000001 << 15);
+
+    addr |= bus;
+    addr <<= 5; // make space
+
+    addr |= slot & 0x1F;
+    addr <<= 3; // make space
+
+    addr |= (func & 0x07);
+    addr <<= 8; // make space
+
+    addr |= offset;
+
+    return addr;
+}
+
+
+
+u32_t PCI_Read32(u32_t addr)
+{
+    outdw(PCI_CONFIG_ADDRESS, addr);
+    io_wait();
+    return indw(PCI_CONFIG_DATA);
+}
