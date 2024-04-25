8204cad8587360f1e351ec000f60662494f88fd4
dumpy <112026806+thatcppnerd@users.noreply.github.com>
2024-04-25 09:01:52 -0500

    starting vga libs

---
+++ include/vga.h
@@ -0,0 +1,41 @@
+#ifndef __VGA_H__
+#define __VGA_H__
+
+#include "types.h"
+
+enum VGA_Ports
+{   
+    VGA_CRTC_CTLR_ADDR =        0x3B4,
+    VGA_CRTC_CTLR_DATA =        0x3B5,
+
+    VGA_INPUT_STATUS1_ALT =     0x3BA, // Read
+    VGA_FEATURE_CTL_ALT1 =      0x3BA, // Write
+
+    VGA_INDEX =                 0x3C0,
+    VGA_DATA =                  0x3C0,
+
+    VGA_ATTRIB_DATA =           0x3C1,
+
+    VGA_INPUT_STATUS0 =         0x3C2, // Read
+    VGA_MISC_OUTPUT =           0x3C2, // Write
+
+    VGA_SEQUENCER_ADDR =        0x3C4,
+
+    VGA_SEQUENCER_DATA =        0x3C5,
+
+    VGA_DAC_MASK =              0x3C6,
+
+    VGA_DAC_STATE =             0x3C7, // Read
+    VGA_DAC_ADDR_READMODE =     0x3C7, // Write
+
+    VGA_DAC_ADDR_WRITEMODE =    0x3C8,
+
+    VGA_DAC_DATA =              0x3C9,
+
+    VGA_FEATURE_CTL_ALT2 =      0x3CA, // Read 
+
+    VGA_INPUT_STATUS1 =         0x3DA, // Read
+    vGA_FEATURE_CTL =           0x3DA // Write
+};
+
+#endif
