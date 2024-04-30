8063bd503ee441ab224c49f57c98c80bc373c914
thatcppnerd <welcometoheccmp4@gmail.com>
2024-04-29 21:35:54 -0500

    pci include guard update

--- include/pci/pci_v3.h
+++ include/pci/pci_v3.h
@@ -1,5 +1,12 @@
-#ifndef __PCI_MMIO_H__
-#define __PCI_MMIO_H__
+#ifndef __PCI_V3_H__
+#define __PCI_V3_H__
+
+#if !(defined(__PCI_V1_H__) || defined(__PCI_V2_H__))
+
+#else
+#pragma message("WARNING: Use of multiple PCI versions detected.")
+#endif
+
 
 
 #endif
