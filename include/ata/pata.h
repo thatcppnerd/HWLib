9ed1f0da938f4accad23cfca1bc157cf9c4b7fc9
thatcppnerd <welcometoheccmp4@gmail.com>
2024-05-12 12:59:10 -0500

    pata wip

--- include/ata/pata.h
+++ include/ata/pata.h
@@ -3,7 +3,7 @@
 
 #include "types.h"
 
-enum ATA_ERROR_REG_Flags
+enum ATA_ErrorReg_Flags
 {
     ATA_ERROR_AMNF =    0x01,
     ATA_ERROR_TKZKF =   0x02,
@@ -14,6 +14,20 @@ enum ATA_ERROR_REG_Flags
     ATA_ERROR_UNC =     0x40,
     ATA_ERROR_BBK =     0x80
 };
+
+enum ATA_StatusReg_Flags
+{
+    ATA_STATUS_ERR =    0x01,
+    ATA_STATUS_IDX =    0x02,
+    ATA_STATUS_CORR =   0x04,
+    ATA_STATUS_DRQ =    0x08,
+    ATA_STATUS_SRV =    0x10,
+    ATA_STATUS_DF =     0x20,
+    ATA_STATUS_RDY =    0x40,
+    ATA_STATUS_BSY =    0x80
+};
+
+
 
 
 #endif
