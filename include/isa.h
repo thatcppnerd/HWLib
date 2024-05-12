a7cd83c5885dd0b26bdcbefba9973cd2c3267939
thatcppnerd <welcometoheccmp4@gmail.com>
2024-05-11 20:24:21 -0500

    isa libs wip

--- include/isa.h
+++ include/isa.h
@@ -12,21 +12,30 @@ const u8_t ISA_Key[32] =
     0xE8, 0x74, 0x3A, 0x9D, 0xCE, 0xE7, 0x73, 0x39
 };
 
+// Current value
+const u16_t ISA_DATA_READ;
+
 enum ISA_Ports
 {
     ISA_ADDRESS =       0x279,
     ISA_DATA_WRITE =    0xA79
 };
 
-enum ISA_CtlSpaceRegs
+enum ISA_ControlSpaceRegs
 {
-    ISA_SET_READ_DATA = 0x00,
-    ISA_SERIAL_OFFSET = 0x01
+    ISA_SET_READ_DATA =     0x00,
+    ISA_SERIAL_ISOLATION =  0x01,
+    ISA_CONFIG_CTL =        0x02,
+    ISA_WAKE =              0x03,
+    ISA_RESOURCE_DATA =     0x04,
+    ISA_STATUS =            0x05,
+    ISA_CARD_SEL_NUM =      0x06,
+    ISA_LOGICAL_DEVICE =    0x07
 };
 
+#define ISA_GetDataReadPort() (ISA_DATA_READ)
 void    ISA_SetDataReadPort(u16_t port);
 
-
-
+// TODO: Serial Isolation Funcs
 
 #endif
