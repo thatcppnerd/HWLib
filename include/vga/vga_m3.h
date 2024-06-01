76451ff1a37f129303fb751280fa68e7eed4466a
thatcppnerd <welcometoheccmp4@gmail.com>
2024-06-01 18:26:52 -0500

    wip

---
+++ include/vga/vga_m3.h
@@ -0,0 +1,54 @@
+#ifndef __VGA_MODE3_H__
+#define __VGA_MODE3_H__
+
+#include "../types.h"
+
+#define VGA_M3_ROWS (25)
+#define VGA_M3_COLS (80)
+
+#define VGA_M3_FB   ((struct VGA_M3_Char*)0xB8000)
+
+#define VGA_M3_ColorByte(bg, fg) ((bg << 4) | fg)
+
+enum VGA_M3_ColorVal
+{
+    VGA_M3_BLACK            = 0,
+    VGA_M3_BLUE             = 1,
+    VGA_M3_GREEN            = 2,
+    VGA_M3_CYAN             = 3,
+    VGA_M3_RED              = 4,
+    VGA_M3_MAGENTA          = 5,
+    VGA_M3_BROWN            = 6,
+    VGA_M3_LIGHT_GREY       = 7,
+    VGA_M3_DARK_GREY        = 8,
+    VGA_M3_LIGHT_BLUE       = 9,
+    VGA_M3_LIGHT_GREEN      = 10,
+    VGA_M3_LIGHT_CYAN       = 11,
+    VGA_M3_LIGHT_RED        = 12,
+    VGA_M3_LIGHT_MAGENTA    = 13,
+    VGA_M3_LIGHT_BROWN      = 14,
+    VGA_M3_WHITE            = 15
+};
+
+struct VGA_M3_Char
+{
+    u8_t chr;
+    u8_t color; // hi nibble is bg, lo nibble is fg
+};
+
+void VGA_M3_SetChar(int x, int y, struct VGA_M3_Char chr);
+struct VGA_M3_Char VGA_M3_GetChar(int x, int y); 
+
+void VGA_M3_SetCharVal(int x, int y, char val);
+char VGA_M3_GetCharVal(int x, int y);
+
+void VGA_M3_SetCharColor(int x, int y, u8_t color);
+u8_t VGA_M3_GetCharColor(int x, int y);
+
+void VGA_M3_Print(int x, int y, char* str, u8_t color);
+
+void VGA_M3_ClearScreen(u8_t color);
+
+
+
+#endif
---
+++ src/vga/vga_m3.c
@@ -0,0 +1,48 @@
+#include "../../include/vga/vga_m3.h"
+
+void VGA_M3_SetChar(int x, int y, struct VGA_M3_Char chr)
+{
+    VGA_M3_FB[(y * VGA_M3_COLS) + x] = chr;
+}
+
+struct VGA_M3_Char VGA_M3_GetChar(int x, int y)
+{
+    return VGA_M3_FB[(y * VGA_M3_COLS) + x];
+}
+
+
+void VGA_M3_SetCharVal(int x, int y, char val)
+{
+    VGA_M3_FB[(y * VGA_M3_COLS) + x].chr = val;
+}
+
+char VGA_M3_GetCharVal(int x, int y)
+{
+    return VGA_M3_FB[(y * VGA_M3_COLS) + x].chr;
+}
+
+void VGA_M3_SetCharColor(int x, int y, u8_t color)
+{
+    VGA_M3_FB[(y * VGA_M3_COLS) + x].color = color;
+}
+
+u8_t VGA_M3_GetCharColor(int x, int y)
+{
+    return VGA_M3_FB[(y * VGA_M3_COLS) + x].color;
+}
+
+void VGA_M3_Print(int x, int y, char* str, u8_t color)
+{
+    for(int c = 0 ; str[c] != 0 ; c++)
+    {
+        VGA_M3_SetChar(x, y, (struct VGA_M3_Char){str[c], color});
+    }
+}
+
+void VGA_M3_ClearScreen(u8_t color)
+{
+    for(int a = 0 ; a < VGA_M3_ROWS * VGA_M3_COLS ; a++)
+    {
+        VGA_M3_FB[a] = (struct VGA_M3_Char){0, color};
+    }
+}
